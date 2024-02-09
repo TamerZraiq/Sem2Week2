796c01d8bc19690b64be9ffba978fa0f73ea0d08
Tamer Zraiq <G00425053@atu.ie>
2024-02-09 12:22:38 +0000

    Added another menu item Dessert

---
+++ src/main/java/ie/atu/Dessert.java
@@ -0,0 +1,25 @@
+package ie.atu;
+
+public class Dessert implements MenuItem{
+    private String name;
+    private double price;
+    private String ingredients;
+
+    public Dessert(String name, double price, String ingredients) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public double getPrice() {
+        return price;
+    }
+    @Override
+    public String getDescription() {
+        return ingredients;
+    }
+}
